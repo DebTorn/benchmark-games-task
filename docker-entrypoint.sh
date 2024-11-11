cf7b35173b39efdb08f01a8773e59de24935c718
Tabori Bence <tabori.bence@skillbot.hu>
2024-11-11 14:34:56 +0100

    Migrate from requirements.txt to pyproject.toml for dependency management and update Dockerfile to use Poetry

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,11 +2,11 @@
 set -e
 
 echo "Migrate Database"
-if [[ $(python3 -m alembic heads) == $(python3 -m alembic current) ]]; then
+if [[ $(poetry run python3 -m alembic heads) == $(poetry run python3 -m alembic current) ]]; then
     echo "No migrations to apply"
 else
-    python3 -m alembic upgrade head
+    poetry run python3 -m alembic upgrade head
 fi
 
 echo "Start Application"
-python3 -m uvicorn src.main:app --host=0.0.0.0 --port=8000
+poetry run python3 -m uvicorn src.main:app --host=0.0.0.0 --port=8000
